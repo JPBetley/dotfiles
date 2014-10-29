ce4c83b16c426a6e53490861b76fa03e871399bf
John Phillip Betley <jpbetley@gmail.com>
2014-10-28 22:54:16 -0400

    updated git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,9 +9,11 @@ fi
 # The rest of my fun git aliases
 alias ga='git add'
 alias gaa='git add -A'
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gpll='git pull'
+alias gpllp='git pull --prune'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gpsh='git push origin HEAD'
+alias gpshu='git push -u origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
@@ -20,3 +22,4 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
