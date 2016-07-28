e87d519ce003c98d24295385e1a4c2023980ff91
John Betley <jpbetley@gmail.com>
2016-07-28 13:46:40 -0400

    added nah git alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -23,4 +23,5 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias nah='git reset --hard;git clean -df;'
 
