72e7d15809e1a418435a74ba8cb911bcdda6e3e1
Phil Betley <jpbetley@gmail.com>
2016-09-22 09:16:27 -0400

    more updates from holman

--- system/env.zsh
+++ system/env.zsh
@@ -1,4 +1,1 @@
-export EDITOR='vim'
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/tatsumori/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+export EDITOR='subl'
